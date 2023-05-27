2357239f5e9288d15ea1615c7b26ee2f179d88a9
GabriellaSwenson <unipuppydj@gmail.com>
2023-05-27 10:50:29 -0500

    html start and init start

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const promptManager = () => {
     },
     {
       type: "input",
-      name: "email",
+      name: "emailManager",
       message: "What is their email?",
     },
     {
@@ -27,7 +27,7 @@ const promptManager = () => {
   ]);
 };
 
-const promptEmployee = () => {
+const promptNewEmployee = () => {
   return inquirer.prompt([
     {
       type: "checkbox",
@@ -35,6 +35,11 @@ const promptEmployee = () => {
       name: "newEmployee",
       choices: ["Yes", "No"],
     },
+  ]);
+};
+
+const promptEmployeeType = () => {
+  return inquirer.prompt([
     {
       type: "checkbox",
       message: "Which employee type would you like to add?",
@@ -96,9 +101,129 @@ const promptIntern = () => {
 
 const init = () => {
   promptManager()
-    .then((answers) => writeFile("index.html", generateHTML(answers)))
-    .then(() => console.log("Successfully wrote to index.html"))
+    .then((answersManager) =>
+      writeFile("index.html", generateManager(answersManager))
+    )
+    .then(() => {
+      if (promptNewEmployee.includes("Yes")) {
+        return inquirer.prompt(promptEmployeeType);
+      } else {
+        console.log("Program closed.");
+        return Promise.resolve();
+      }
+    })
     .catch((err) => console.error(err));
 };
 
 init();
+
+const generateIntern = (promptIntern) => {
+  return `
+                <div class="card m-5 shadow p-0" style="width:300px">
+                    <div class="card-header bg-dark text-white">
+                        <h3>${promptIntern.intern()}</h3>
+                        <h4>
+                        <i class="fas fa-user-graduate pr-3"></i>${promptIntern.getIntern()}
+                        </h4>
+                    </div>
+                    <div class="card-body bg-light">
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <span class="font-weight-bold">ID:</span> ${promptIntern.Id()}</li>
+                            <li class="list-group-item">
+                                <span class="font-weight-bold">Email:</span><a href="mailto:${promptIntern.emailIntern()}">${promptIntern.emailIntern()}</a>
+                            </li>
+                            <li class="list-group-item">
+                                <span class="font-weight-bold">School:</span>${promptIntern.school()}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+    `;
+};
+
+const generateEngineer = (promptEngineer) => {
+  return `
+                  <div class="card m-5 shadow p-0" style="width:300px">
+                      <div class="card-header bg-dark text-white">
+                          <h3>${promptEngineer.engineer()}</h3>
+                          <h4>
+                          <i class="fas fa-user-graduate pr-3"></i>${promptEngineer.getEngineer()}
+                          </h4>
+                      </div>
+                      <div class="card-body bg-light">
+                          <ul class="list-group">
+                              <li class="list-group-item">
+                                  <span class="font-weight-bold">ID:</span> ${promptEngineer.Id()}</li>
+                              <li class="list-group-item">
+                                  <span class="font-weight-bold">Email:</span><a href="mailto:${promptEngineer.emailEngineer()}">${promptEngineer.emailEngineer()}</a>
+                              </li>
+                              <li class="list-group-item">
+                                  <span class="font-weight-bold">School:</span>${promptEngineer.github()}
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+      `;
+};
+
+const generateManager = (promptManager) => {
+  return `
+                  <div class="card m-5 shadow p-0" style="width:300px">
+                      <div class="card-header bg-dark text-white">
+                          <h3>${promptManager.manager()}</h3>
+                          <h4>
+                          <i class="fas fa-user-graduate pr-3"></i>${promptManager.getManager()}
+                          </h4>
+                      </div>
+                      <div class="card-body bg-light">
+                          <ul class="list-group">
+                              <li class="list-group-item">
+                                  <span class="font-weight-bold">ID:</span> ${promptManager.Id()}</li>
+                              <li class="list-group-item">
+                                  <span class="font-weight-bold">Email:</span><a href="mailto:${promptManager.emailManager()}">${promptManager.emailManager()}</a>
+                              </li>
+                              <li class="list-group-item">
+                                  <span class="font-weight-bold">School:</span>${promptManager.officeNumber()}
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+      `;
+};
+
+const generatePage = {
+  generateManager,
+  generateEngineer,
+  generateIntern,
+}`<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    </head>
+    <body>
+        <header>
+            <div class="text-center">
+                <nav class="navabar bg-dark text-white text-center pt-3 pb-5">
+                    <div>
+                        <h1>Team</h1>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row d-flex justify-content-center align-items-center">
+                    ${generateManager}, ${generateEngineer}, ${generateIntern}
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
+    `;
