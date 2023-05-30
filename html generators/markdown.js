c031211aa5c6857a23881a2d2231cef6ab797bbd
GabriellaSwenson <unipuppydj@gmail.com>
2023-05-29 19:56:38 -0500

    markdown file

--- html generators/markdown.js
+++ html generators/markdown.js
@@ -3,74 +3,131 @@ const generatePage = (page) => {
     return `
                         <div class="card m-5 shadow p-0" style="width:300px">
                             <div class="card-header bg-dark text-white">
-                                <h3>${promptManager.manager()}</h3>
+                                <h3>${manager.getName()}</h3>
                                 <h4>
-                                <i class="fas fa-user-graduate pr-3"></i>${promptManager.getManager()}
+                                <i class="fas fa-user-graduate pr-3"></i>${manager.getType()}
                                 </h4>
                             </div>
                             <div class="card-body bg-light">
                                 <ul class="list-group">
                                     <li class="list-group-item">
-                                        <span class="font-weight-bold">ID:</span> ${promptManager.Id()}</li>
+                                        <span class="font-weight-bold">ID:</span> ${manager.getId()}</li>
                                     <li class="list-group-item">
-                                        <span class="font-weight-bold">Email:</span><a href="mailto:${promptManager.emailManager()}">${promptManager.emailManager()}</a>
+                                        <span class="font-weight-bold">Email:</span><a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
                                     </li>
                                     <li class="list-group-item">
-                                        <span class="font-weight-bold">School:</span>${promptManager.officeNumber()}
+                                        <span class="font-weight-bold">School:</span>${manager.getOfficeNumber()}
                                     </li>
                                 </ul>
                             </div>
                         </div>
             `;
   };
-};
 
-const generateEngineer = (engineer) => {
-  return `
+  const generateEngineer = (engineer) => {
+    return `
                     <div class="card m-5 shadow p-0" style="width:300px">
                         <div class="card-header bg-dark text-white">
-                            <h3>${promptEngineer.engineer()}</h3>
+                            <h3>${engineer.getName()}</h3>
                             <h4>
-                            <i class="fas fa-user-graduate pr-3"></i>${promptEngineer.getEngineer()}
+                            <i class="fas fa-user-graduate pr-3"></i>${engineer.getType()}
                             </h4>
                         </div>
                         <div class="card-body bg-light">
                             <ul class="list-group">
                                 <li class="list-group-item">
-                                    <span class="font-weight-bold">ID:</span> ${promptEngineer.Id()}</li>
+                                    <span class="font-weight-bold">ID:</span> ${engineer.getId()}</li>
                                 <li class="list-group-item">
-                                    <span class="font-weight-bold">Email:</span><a href="mailto:${promptEngineer.emailEngineer()}">${promptEngineer.emailEngineer()}</a>
+                                    <span class="font-weight-bold">Email:</span><a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
                                 </li>
                                 <li class="list-group-item">
-                                    <span class="font-weight-bold">School:</span>${promptEngineer.github()}
+                                    <span class="font-weight-bold">School:</span>${engineer.getGithub()}
                                 </li>
                             </ul>
                         </div>
                     </div>
         `;
-};
+  };
 
-const generateIntern = (promptIntern) => {
-  return `
+  const generateIntern = (intern) => {
+    return `
                   <div class="card m-5 shadow p-0" style="width:300px">
                       <div class="card-header bg-dark text-white">
-                          <h3>${promptIntern.intern()}</h3>
+                          <h3>${intern.getName()}</h3>
                           <h4>
-                          <i class="fas fa-user-graduate pr-3"></i>${promptIntern.getIntern()}
+                          <i class="fas fa-user-graduate pr-3"></i>${intern.getType()}
                           </h4>
                       </div>
                       <div class="card-body bg-light">
                           <ul class="list-group">
                               <li class="list-group-item">
-                                  <span class="font-weight-bold">ID:</span> ${promptIntern.Id()}</li>
+                                  <span class="font-weight-bold">ID:</span> ${intern.getId()}</li>
                               <li class="list-group-item">
-                                  <span class="font-weight-bold">Email:</span><a href="mailto:${promptIntern.emailIntern()}">${promptIntern.emailIntern()}</a>
+                                  <span class="font-weight-bold">Email:</span><a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a>
                               </li>
                               <li class="list-group-item">
-                                  <span class="font-weight-bold">School:</span>${promptIntern.school()}
+                                  <span class="font-weight-bold">School:</span>${intern.getSchool()}
                               </li>
                           </ul>
                       </div>
                   </div>
       `;
+  };
+};
+
+const markdown = [];
+
+markdown.push(
+  page
+    .filter((employee) => employee.getType() === "Manager")
+    .map((manager) => generateManager(manager))
+);
+markdown.push(
+  page
+    .filter((employee) => employee.getType() === "Engineer")
+    .map((engineer) => generateEngineer(engineer))
+    .join("")
+);
+markdown.push(
+  page
+    .filter((employee) => employee.getType() === "Intern")
+    .map((intern) => generateIntern(intern))
+    .join("")
+);
+
+return markdown.join("");
+
+module.exports = (page) => {
+  return `<!DOCTYPE html>
+    // <html lang="en">
+    //     <head>
+    //         <meta charset="UTF-8" />
+    //         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    //         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    //         <title>My Team</title>
+    //         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    //         <link rel="stylesheet" href="style.css">
+    //         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    //     </head>
+    //     <body>
+    //         <header>
+    //             <div class="text-center">
+    //                 <nav class="navabar bg-dark text-white text-center pt-3 pb-5">
+    //                     <div>
+    //                         <h1>Team</h1>
+    //                     </div>
+    //                 </nav>
+    //             </div>
+    //         </header>
+    //         <main>
+    //             <div class="container">
+    //                 <div class="row d-flex justify-content-center align-items-center">
+    //                     ${generatePage(page)}
+    //                 </div>
+    //         </div>
+    //             </div>
+    //         </main>
+    //     </body>
+    // </html>
+    //     `;
 };
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const Manager = require("./Manager");
 const Engineer = require("./Engineer");
 const Intern = require("./Intern");
 
-const { writeFile } = require("fs").promises;
-
 let employees = [];
 
 prompts();
@@ -47,7 +45,7 @@ function buildTeam() {
     prompts();
   });
 } else if (response.employeeType === 'Engineer') {
-inquirer.prompt([
+  inquirer.prompt([
       {
       type: "input",
       name: "name",
@@ -102,153 +100,13 @@ inquirer.prompt([
 
     prompts();
   })
+} else if (response.employeeType === 'Finished') {
+  const createPage = generateMarkdown(employees);
+  fs.writeFile('./html generators/markdown.js', createMarkdown(err), (err) => {
+    if (err) throw err;
+  
+    console.log('HTML file has been created!');
+  });  
 }
   })
 }
-
-// const init = () => {
-//   const runEmployeePrompts = () => {
-//     inquirer
-//       .prompt(promptNewEmployee)
-//       .then((answersNewEmployee) => {
-//         if (answersNewEmployee.promptNewEmployee.includes("No")) {
-//           console.log("Program closed."); // You can customize the message
-//           return Promise.resolve(); // Return a resolved promise to continue the chain
-//         } else {
-//           return inquirer.prompt(promptEmployeeType);
-//         }
-//       })
-//       .then((answersEmployeeType) => {
-//         const { employeeType } = answersEmployeeType;
-
-//         if (employeeType === "engineer") {
-//           return inquirer.prompt(promptEngineer).then(runEmployeePrompts);
-//         } else if (employeeType === "intern") {
-//           return inquirer.prompt(promptIntern).then(runEmployeePrompts);
-//         } else {
-//           console.log("Invalid employee type."); // You can customize the message
-//           return runEmployeePrompts();
-//         }
-//       })
-//       .catch((err) => console.error(err));
-//   };
-
-//   promptManager()
-//     .then((answersManager) =>
-//       writeFile("index.html", generateManager(answersManager))
-//     )
-//     .then(runEmployeePrompts)
-//     .catch((err) => console.error(err));
-// };
-
-// init();
-
-// const generateManager = (promptManager) => {
-//   return `
-//                   <div class="card m-5 shadow p-0" style="width:300px">
-//                       <div class="card-header bg-dark text-white">
-//                           <h3>${promptManager.manager()}</h3>
-//                           <h4>
-//                           <i class="fas fa-user-graduate pr-3"></i>${promptManager.getManager()}
-//                           </h4>
-//                       </div>
-//                       <div class="card-body bg-light">
-//                           <ul class="list-group">
-//                               <li class="list-group-item">
-//                                   <span class="font-weight-bold">ID:</span> ${promptManager.Id()}</li>
-//                               <li class="list-group-item">
-//                                   <span class="font-weight-bold">Email:</span><a href="mailto:${promptManager.emailManager()}">${promptManager.emailManager()}</a>
-//                               </li>
-//                               <li class="list-group-item">
-//                                   <span class="font-weight-bold">School:</span>${promptManager.officeNumber()}
-//                               </li>
-//                           </ul>
-//                       </div>
-//                   </div>
-//       `;
-// };
-
-// const generateEngineer = (promptEngineer) => {
-//   return `
-//                   <div class="card m-5 shadow p-0" style="width:300px">
-//                       <div class="card-header bg-dark text-white">
-//                           <h3>${promptEngineer.engineer()}</h3>
-//                           <h4>
-//                           <i class="fas fa-user-graduate pr-3"></i>${promptEngineer.getEngineer()}
-//                           </h4>
-//                       </div>
-//                       <div class="card-body bg-light">
-//                           <ul class="list-group">
-//                               <li class="list-group-item">
-//                                   <span class="font-weight-bold">ID:</span> ${promptEngineer.Id()}</li>
-//                               <li class="list-group-item">
-//                                   <span class="font-weight-bold">Email:</span><a href="mailto:${promptEngineer.emailEngineer()}">${promptEngineer.emailEngineer()}</a>
-//                               </li>
-//                               <li class="list-group-item">
-//                                   <span class="font-weight-bold">School:</span>${promptEngineer.github()}
-//                               </li>
-//                           </ul>
-//                       </div>
-//                   </div>
-//       `;
-// };
-
-// const generateIntern = (promptIntern) => {
-//   return `
-//                 <div class="card m-5 shadow p-0" style="width:300px">
-//                     <div class="card-header bg-dark text-white">
-//                         <h3>${promptIntern.intern()}</h3>
-//                         <h4>
-//                         <i class="fas fa-user-graduate pr-3"></i>${promptIntern.getIntern()}
-//                         </h4>
-//                     </div>
-//                     <div class="card-body bg-light">
-//                         <ul class="list-group">
-//                             <li class="list-group-item">
-//                                 <span class="font-weight-bold">ID:</span> ${promptIntern.Id()}</li>
-//                             <li class="list-group-item">
-//                                 <span class="font-weight-bold">Email:</span><a href="mailto:${promptIntern.emailIntern()}">${promptIntern.emailIntern()}</a>
-//                             </li>
-//                             <li class="list-group-item">
-//                                 <span class="font-weight-bold">School:</span>${promptIntern.school()}
-//                             </li>
-//                         </ul>
-//                     </div>
-//                 </div>
-//     `;
-// };
-
-// const generatePage = () => {
-//   `<!DOCTYPE html>
-// <html lang="en">
-//     <head>
-//         <meta charset="UTF-8" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-//         <title>My Team</title>
-//         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-//         <link rel="stylesheet" href="style.css">
-//         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-//     </head>
-//     <body>
-//         <header>
-//             <div class="text-center">
-//                 <nav class="navabar bg-dark text-white text-center pt-3 pb-5">
-//                     <div>
-//                         <h1>Team</h1>
-//                     </div>
-//                 </nav>
-//             </div>
-//         </header>
-//         <main>
-//             <div class="container">
-//                 <div class="row d-flex justify-content-center align-items-center">
-//                     ${generateManager}, ${generateEngineer}, ${generateIntern}
-//                 </div>
-//         </div>
-//             </div>
-//         </main>
-//     </body>
-// </html>
-//     `;
-// };
